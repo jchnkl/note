5eaff1b7547d5a9bdd075645f5fb44d94d2dda57
Jochen Keil <jochen.keil@gmail.com>
2015-09-01 17:50:45 +0200

    Don't allow empty dates

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -4,22 +4,24 @@
  */
 
 /* Table for note meta information */
-CREATE TABLE IF NOT EXISTS notes_meta(
-  note_meta_id   CHAR(65) NOT NULL UNIQUE,
-  note_meta_date CHAR(25),
-  note_meta_name TEXT
+CREATE TABLE IF NOT EXISTS notes_info(
+  id   CHAR(65) NOT NULL UNIQUE,
+  date CHAR(25) NOT NULL,
+  title TEXT
+);
+
 );
 
 /* Table for the actual note */
-CREATE TABLE IF NOT EXISTS notes_note(
-  note_id      CHAR(65) NOT NULL UNIQUE,
-  note_content BLOB,
-  FOREIGN KEY(note_id) REFERENCES notes_meta(note_meta_id)
+CREATE TABLE IF NOT EXISTS notes_content(
+  id      CHAR(65) NOT NULL UNIQUE,
+  content BLOB,
+  FOREIGN KEY(id) REFERENCES notes_info(id)
 );
 
 /* Table for attachments */
 CREATE TABLE IF NOT EXISTS notes_attachments(
-  attachment_id      CHAR(65) NOT NULL,
-  attachment_content BLOB,
-  FOREIGN KEY(attachment_id) REFERENCES notes_meta(note_meta_id)
+  id      CHAR(65) NOT NULL,
+  content BLOB,
+  FOREIGN KEY(id) REFERENCES notes_info(id)
 );
