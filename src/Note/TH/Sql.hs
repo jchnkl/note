a07c9847843a4da07aa6342f0407ded0e2be37e3
Jochen Keil <jochen.keil@gmail.com>
2015-09-02 13:51:46 +0200

    Read sql schema at compile time with TH

---
+++ src/Note/TH/Sql.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Note.TH.Sql where
+
+import Control.Monad.IO.Class
+import Language.Haskell.TH
+
+mkSqlSchema :: Q [Dec]
+mkSqlSchema = do
+    schema <- runIO $ readFile "src/sql/schema.sql"
+    [d|sqlSchema = schema|]
